bd8e5eb39ee8d992d2d2f41b91bd13e86e7666e9
TomFelix <tomasz.agacinski@gmail.com>
2018-06-21 09:21:33 +0200

    input test

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      data: []
+    };
+  }
+
+
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <TextInput value={this.state.term} onChangeText={(text) => this.setState({term: text})} placeholder="Put name here..."/>
+        <Text>{this.state.term}</Text>
+
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
@@ -19,5 +32,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10
   },
 });
